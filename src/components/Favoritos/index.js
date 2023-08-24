2fe564e2ad847bd27371e3247d5d4acfef18ca73
Raí <megac154@gmail.com>
2023-08-24 12:02:53 -0300

    component Favoritos and Container with router

---
+++ src/components/Favoritos/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Favoritos.module.css'
+
+export default function Favoritos() {
+  return (
+    <div>Favoritos</div>
+  )
+}
--- src/router.js
+++ src/router.js
@@ -1,3 +1,7 @@
+import Cabecalho from 'components/Cabecalho'
+import Container from 'components/Container'
+import Favoritos from 'components/Favoritos'
+import Rodape from 'components/Rodape'
 import Inicio from 'pages/Inicio'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -5,10 +9,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Inicio />}>
-        </Route>
-      </Routes>
+      <Cabecalho />
+        <Container>
+          <Routes>
+            <Route path='/' element={<Inicio />}></Route>
+            <Route path='/favoritos' element={<Favoritos />}></Route>
+          </Routes>
+        </Container>
+      <Rodape />
     </BrowserRouter>
   )
 }
