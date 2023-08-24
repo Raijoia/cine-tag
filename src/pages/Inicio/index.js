f9dd0ba1ead93e53b61ee32427ab963f84dcc9df
Raí <megac154@gmail.com>
2023-08-24 11:46:44 -0300

    component Card

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,11 @@
 import Banner from 'components/Banner'
 import Cabecalho from 'components/Cabecalho'
+import Card from 'components/Card'
 import Rodape from 'components/Rodape'
 import Titulo from 'components/Titulo'
 import React from 'react'
+import videos from 'json/db.json'
+import styles from './Inicio.module.css'
 
 export default function Inicio() {
   return (
@@ -12,6 +15,12 @@ export default function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          // express manda as características de cada object
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
       <Rodape />
     </>
   )
