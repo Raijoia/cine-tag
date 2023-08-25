25ea401af0e159cae9219c2dbf019fae4985d0c8
Raí <megac154@gmail.com>
2023-08-25 12:08:11 -0300

    function adicionarFavorito

--- src/context/Favoritos.js
+++ src/context/Favoritos.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
@@ -18,4 +19,32 @@ export default function FavoritosProvider({ children }) {
       {children}
     </FavoritosContext.Provider>
   )
+}
+
+export function useFavoritoContext() {
+  // pegando o favorito e o setFavorito do context
+  const { favorito, setFavorito } = useContext(FavoritosContext)
+
+  function adicionarFavorito(novoFavorito) {
+    // verifica se já tem na lista
+    const favoritoRepetido = favorito.some(item => item.id === novoFavorito.id);
+
+    // colocando favoritos em uma nova lista
+    let novaLista = [...favorito]
+
+    // adiciona para a lista se não for repetido e retorna a nova lista pro setFavorito
+    if(!favoritoRepetido) {
+      novaLista.push(novoFavorito)
+      return setFavorito(novaLista)
+    }
+
+    // filtra a lista, removendo apenas o elemento repetido
+    novaLista = favorito.filter((fav) => fav.id !== novoFavorito.id)
+    return setFavorito(novaLista)
+  }
+
+  return {
+    favorito,
+    adicionarFavorito
+  }
 }
